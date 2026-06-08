1cfa6d12b208411385bed962132239facfd605fb
gumushobeangel-del <gumushobeangel@gmail.com>
2026-06-08 18:41:25 +0300

    clean project for deployment

--- hardwareproject/hardwareproject/settings.py
+++ hardwareproject/hardwareproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-3%ey+ppbj$gbj50(%txoi6-jl1=(hhmwpaldq4p)(fznqn$aw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
